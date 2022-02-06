327ff3c256e75c515eea5bc5bddddc7f1f5d613b
vasiliy <vasyaburton91@gmail.com>
2022-02-06 12:14:19 +0300

    Initial commit

---
+++ VK_SwiftUI_/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VK_SwiftUI_
+//
+//  Created by Василий on 06.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VK_SwiftUI_/VK_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUI_App.swift
+//  VK_SwiftUI_
+//
+//  Created by Василий on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
